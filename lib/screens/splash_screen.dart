c62087520a1eec986bc607e8dae9c68190a3fbb6
AGC <anthonygcv3@gmail.com>
2024-03-29 18:28:21 -0600

    Primer commit conteniendo la estructura general y con el splashscreen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:renta_movil_app/screens/dashboard_screen.dart';
+import 'package:splash_view/source/presentation/pages/pages.dart';
+import 'package:splash_view/source/presentation/widgets/done.dart';
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SplashView(
+      backgroundColor: Colors.black,
+      loadingIndicator: Image.asset('images/loading.gif'),
+      done: Done(const DashboardScreen(),
+      animationDuration: const Duration(milliseconds: 3000)
+      ),
+    );
+  }
+}
