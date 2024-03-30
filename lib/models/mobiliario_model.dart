1e3a778d7aa8c94e03cd997993ef2b56554d12cb
AGC <anthonygcv3@gmail.com>
2024-03-30 16:37:55 -0600

    Se agrego el modelado completo de la base de datos asi como tambien la estructura de la misma en mobiliario_database, los cruds estan listos ahora se necesita una interfaz para su uso, quiza se tengan que anadir algunos metodos en el futuro pero es necesario ver. Ademas se necesitara probar a profundidad los cruds de las tablas n-n

--- lib/models/mobiliario_model.dart
+++ lib/models/mobiliario_model.dart
@@ -4,7 +4,7 @@ class MobiliarioModel {
   int? cantidadTotalMobiliario;
   int? cantidadDisponibleMobiliario;
   String? descripcionMobiliario;
-  String? precioRentaMobiliario;
+  double? precioRentaMobiliario;
   MobiliarioModel(
       {this.idMobiliario,
       this.nombreMobiliario,
@@ -12,24 +12,48 @@ class MobiliarioModel {
       this.cantidadDisponibleMobiliario,
       this.descripcionMobiliario,
       this.precioRentaMobiliario});
+  factory MobiliarioModel.fromMap(Map<String, dynamic> mobiliario) {
+    return MobiliarioModel(
+        idMobiliario: mobiliario['idMobiliario'],
+        nombreMobiliario: mobiliario['nombreMobiliario'],
+        cantidadTotalMobiliario: mobiliario['cantidadTotalMobiliario'],
+        cantidadDisponibleMobiliario:
+            mobiliario['cantidadDisponibleMobiliario'],
+        descripcionMobiliario: mobiliario['descripcionMobiliario'],
+        precioRentaMobiliario: mobiliario['precioRentaMobiliario']);
+  }
 }
 
 class CategoriaModel {
   int? idCategoria;
   String? nombreCategoria;
   CategoriaModel({this.idCategoria, this.nombreCategoria});
+  factory CategoriaModel.fromMap(Map<String, dynamic> categoria) {
+    return CategoriaModel(
+        idCategoria: categoria['idCategoria'],
+        nombreCategoria: categoria['nombreCategoria']);
+  }
 }
 
 class MobiliarioCategoriaModel {
   int? idMobiliario;
   int? idCategoria;
   MobiliarioCategoriaModel({this.idMobiliario, this.idCategoria});
+  factory MobiliarioCategoriaModel.fromMap(Map<String, dynamic> mobcategoria) {
+    return MobiliarioCategoriaModel(
+        idMobiliario: mobcategoria['idMobiliario'],
+        idCategoria: mobcategoria['idCategoria']);
+  }
 }
 
 class StatusModel {
   int? idStatus;
   String? nombreStatus;
   StatusModel({this.idStatus, this.nombreStatus});
+  factory StatusModel.fromMap(Map<String, dynamic> status) {
+    return StatusModel(
+        idStatus: status['idStatus'], nombreStatus: status['nombreStatus']);
+  }
 }
 
 class RentaModel {
@@ -40,7 +64,8 @@ class RentaModel {
   String? nombreRenta;
   String? telefonoRenta;
   String? direccionRenta;
-  String? montoRenta;
+  double? montoRenta;
+  int? idStatus;
   RentaModel(
       {this.idRenta,
       this.fechaInicioRenta,
@@ -49,19 +74,39 @@ class RentaModel {
       this.nombreRenta,
       this.telefonoRenta,
       this.direccionRenta,
-      this.montoRenta});
+      this.montoRenta,
+      this.idStatus});
+  factory RentaModel.fromMap(Map<String, dynamic> renta) {
+    return RentaModel(
+        idRenta: renta['idRenta'],
+        fechaInicioRenta: renta['fechaInicioRenta'],
+        //Podria usar lo siguente despues:
+        //fechaInicio: renta['fechaInicio'] != null ? DateTime.parse(renta['fechaInicio']) : null,
+        fechaFinRenta: renta['fechaFinRenta'],
+        fechaEntregaRenta: renta['fechaEntregaRenta'],
+        nombreRenta: renta['nombreRenta'],
+        telefonoRenta: renta['telefonoRenta'],
+        direccionRenta: renta['direccionRenta'],
+        montoRenta: renta['montoRenta'],
+        idStatus: renta['idStatus']);
+  }
 }
 
 class RentaDetalleModel {
-  int? idDetalle;
   int? idRenta;
   int? idMobiliario;
   int? cantidadDetalle;
-  String? precioUnitarioDetalle;
+  double? precioUnitarioDetalle;
   RentaDetalleModel(
-      {this.idDetalle,
-      this.idRenta,
+      {this.idRenta,
       this.idMobiliario,
       this.cantidadDetalle,
       this.precioUnitarioDetalle});
+  factory RentaDetalleModel.fromMap(Map<String, dynamic> rendetalle) {
+    return RentaDetalleModel(
+        idRenta: rendetalle['idRenta'],
+        idMobiliario: rendetalle['idMobiliario'],
+        cantidadDetalle: rendetalle['cantidadDetalle'],
+        precioUnitarioDetalle: rendetalle['precioUnitarioDetalle']);
+  }
 }
